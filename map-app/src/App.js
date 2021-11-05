190ac248b36acd662afb41a9d08c958f18e49daa
Ramis <ramis.gaynetdinov@gmail.com>
2021-11-05 11:38:31 +0100

    refresh_map

--- map-app/src/App.js
+++ map-app/src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useLayoutEffect, useState } from "react";
+import { render } from "react-dom";
+import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
 
-function App() {
+export default function App() {
+  const [random, setRandom] = useState(Math.random());
+
+  mapboxgl.accessToken = "pk.eyJ1IjoicmFyYW0iLCJhIjoiY2t2bGRtNWN6M2ZtbzJvcGd2ZDV5eTBmMiJ9.H3XDB5QalWUljh2wEWg-iA";
+
+  useLayoutEffect(() => {
+      const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: [37.61192, 55.76199],
+      zoom: 10
+    })
+      return () => {
+
+      }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button id="rerender" onClick={() => setRandom(Math.random())}>
+        Ререндер!
+      </button>
+      <div id="map"></div>
+    </>
   );
 }
 
-export default App;
+render(<App />, document.querySelector("#root"));
